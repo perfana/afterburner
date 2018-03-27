323a2e92ea006d517fe4012751ca3aa93f3f3083
Peter Paul Bakker <peter.paul.bakker@stokpop.nl>
2018-03-27 14:56:50 +0200

    First commit with just generated start.spring.io artifacts.

---
+++ src/main/java/com/stokpop/afterburner/AfterburnerApplication.java
@@ -0,0 +1,12 @@
+package com.stokpop.afterburner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfterburnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AfterburnerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/stokpop/afterburner/AfterburnerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.stokpop.afterburner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AfterburnerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
