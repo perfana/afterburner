04ea8958ada8ff90c01c4defc96aaaa9993becd5
Peter Paul Bakker <peter.paul.bakker@stokpop.nl>
2019-12-14 13:51:29 +0100

    added new requests to gatling test, added fix for jvm metrics in prometheus scraper

--- afterburner-java/src/main/java/nl/stokpop/afterburner/config/AfterburnerAsyncConfig.java
+++ afterburner-java/src/main/java/nl/stokpop/afterburner/config/AfterburnerAsyncConfig.java
@@ -2,6 +2,10 @@ package nl.stokpop.afterburner.config;
 
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.binder.jvm.ExecutorServiceMetrics;
+import io.micrometer.prometheus.PrometheusMeterRegistry;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
@@ -32,5 +36,14 @@ public class AfterburnerAsyncConfig implements AsyncConfigurer {
         // enable metrics for this executor
         return ExecutorServiceMetrics.monitor(registry, executor.getThreadPoolExecutor(), executorName);
     }
+
+    // needed to reactivate the jvm metrics after registering executor (???)
+    // tip from stackoverflow below, but that is about security
+    // https://stackoverflow.com/questions/57607445/spring-actuator-jvm-metrics-not-showing-when-globalmethodsecurity-is-enabled
+    @Bean
+    InitializingBean forcePrometheusPostProcessor(BeanPostProcessor meterRegistryPostProcessor, PrometheusMeterRegistry registry) {
+        return () -> meterRegistryPostProcessor.postProcessAfterInitialization(registry, "");
+    }
+    
 
 }
