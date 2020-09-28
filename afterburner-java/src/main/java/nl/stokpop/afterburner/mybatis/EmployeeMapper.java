aaf348ee968a068b8bbd81a5843769221fa97583
Peter Paul Bakker <peter.paul.bakker@stokpop.nl>
2020-09-28 10:37:56 +0200

    disable mybatis cache, uncomment to enable again!

--- afterburner-java/src/main/java/nl/stokpop/afterburner/mybatis/EmployeeMapper.java
+++ afterburner-java/src/main/java/nl/stokpop/afterburner/mybatis/EmployeeMapper.java
@@ -1,6 +1,5 @@
 package nl.stokpop.afterburner.mybatis;
 
-import org.apache.ibatis.annotations.CacheNamespace;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
@@ -8,7 +7,8 @@ import org.apache.ibatis.annotations.Select;
 import java.util.List;
 
 @Mapper
-@CacheNamespace(readWrite = false)
+// uncomment next line to activate mybatis cache
+// @CacheNamespace(readWrite = false)
 public interface EmployeeMapper {
     @Select("SELECT * FROM employees.employees WHERE first_name = #{firstName}")
     List<Employee> selectEmployeeWithFirstName(@Param("firstName") String firstName);
