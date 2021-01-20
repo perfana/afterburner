d5d5fe102df15dd3e57d9f348fa7ef6bf5e790cb
Matthijn Huijgens <matthijnhuijgens@utcl104121.eu.rabonet.com>
2021-01-20 15:56:36 +0100

    added 3 javaclasses to measure custom http throughput

--- afterburner-java/src/main/java/nl/stokpop/afterburner/metrics/RequestStatistics.java
+++ afterburner-java/src/main/java/nl/stokpop/afterburner/metrics/RequestStatistics.java
@@ -1,2 +1,17 @@
-package nl.stokpop.afterburner.metrics;public class RequestStatistics {
+package nl.stokpop.afterburner.metrics;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+
+@Slf4j
+@RequiredArgsConstructor
+class RequestStatistics {
+
+    public final CustomThroughputGauge customThroughputGauge;
+
+    @Scheduled(fixedDelayString = "${app.statistics.throughput.schedule:1000}")
+    void measure() {
+        customThroughputGauge.setRps();
+    }
 }
