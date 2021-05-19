05c3690abfe12e40bf51d839610966a17bd2dbab
Peter Paul Bakker <peter.paul.bakker@stokpop.nl>
2021-05-19 23:02:05 +0200

    better defaults for workshop

--- docker/workshop/show-pool-metrics.sh
+++ docker/workshop/show-pool-metrics.sh
@@ -1,8 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
 POOL=employee-db-pool
-echo --- hikariCP pool: $POOL
-echo "aquireTotalTime: $(curl -Ss http://localhost:8080/actuator/metrics/hikaricp.connections.acquire\?tag\=pool:$POOL | jq -r ' .measurements[] | select(.statistic=="TOTAL_TIME") | .value ')"
+HOST=${1:-localhost}
 
-echo "idle           : $(curl -Ss http://localhost:8080/actuator/metrics/hikaricp.connections.idle?tag=pool:$POOL | jq -r ' .measurements[] | .value ')"
-echo "active         : $(curl -Ss http://localhost:8080/actuator/metrics/hikaricp.connections.active?tag=pool:$POOL | jq -r ' .measurements[] | .value ')"
+echo --- hikariCP pool: $POOL
+echo "acquireTotalTime: $(curl -Ss http://$HOST:8080/actuator/metrics/hikaricp.connections.acquire\?tag\=pool:$POOL | jq -r ' .measurements[] | select(.statistic=="TOTAL_TIME") | .value ')"
+
+echo "idle           : $(curl -Ss http://$HOST:8080/actuator/metrics/hikaricp.connections.idle?tag=pool:$POOL | jq -r ' .measurements[] | .value ')"
+echo "active         : $(curl -Ss http://$HOST:8080/actuator/metrics/hikaricp.connections.active?tag=pool:$POOL | jq -r ' .measurements[] | .value ')"
+echo "timeouts       : $(curl -Ss http://$HOST:8080/actuator/metrics/hikaricp.connections.timeout?tag=pool:$POOL | jq -r ' .measurements[] | .value ')"
