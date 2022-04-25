f8cec35c79c05508a31f12a1bc90e1aa5142e50a
Peter Paul Bakker <peter.paul.bakker@stokpop.nl>
2022-04-25 09:59:47 +0200

    updated docker compose environment

--- docker/download-byteman.sh
+++ docker/download-byteman.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
-BYTEMAN_URL="https://downloads.jboss.org/byteman/4.0.14/byteman-download-4.0.14-bin.zip"
+# fail on error
+set -e
+
+BYTEMAN_URL="https://downloads.jboss.org/byteman/4.0.18/byteman-download-4.0.18-bin.zip"
 BYTEMAN_ZIP=$(basename $BYTEMAN_URL)
 
 MAIN_DIR=$( echo $BYTEMAN_ZIP | sed "s/^\(.*\)-bin.zip$/\1/" )
 
 cd byteman || (echo "byteman dir not found!"; exit 2)
 echo "download $BYTEMAN_URL"
-curl -Ss -O -L $BYTEMAN_URL
+curl -O -L $BYTEMAN_URL
 echo "unzip $BYTEMAN_ZIP"
 unzip $BYTEMAN_ZIP
 rm -v $BYTEMAN_ZIP
