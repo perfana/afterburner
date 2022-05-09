35c391a3adaa54559ad27f7435e1b49e76081e90
Peter Paul Bakker <peter.paul.bakker@stokpop.nl>
2022-05-09 20:06:55 +0200

    workshop: disable download for employee-db when not needed

--- docker/start.sh
+++ docker/start.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 
+export COMPOSE_PROJECT_NAME=workshop
+
+ACTIVATE_EMPLOYEE_DB=false
+
 # update wrk2 image for apple silicon
 if [[ $(uname -m) == 'arm64' ]]; then
   echo "Detected Apple Silicon M1, update wrk2 to M1 compatible container"
   sed -i '' 's/wrk2:0.0.3/wrk2-aarch:0.0.2/g' docker-compose.yml
 fi
  
-
 mkdir -p -v dumps
 mkdir -p -v byteman
-mkdir -p -v mariadb
+if [ "$ACTIVATE_EMPLOYEE_DB" = true ]; then
+    mkdir -p -v mariadb
+fi
 
-if [ ! -f employee-db/test_db/employees.sql ]; then
+if [[ ! -f employee-db/test_db/employees.sql && "$ACTIVATE_EMPLOYEE_DB" = true ]]; then
     echo "First download and unpack employees db"
     ./download-emp-db.sh
 fi
 
-export COMPOSE_PROJECT_NAME=workshop
-
-#docker compose up -d mariadb
+if [ "$ACTIVATE_EMPLOYEE_DB" = true ]; then
+    docker compose up -d mariadb
+fi
 docker compose up -d jaeger
 docker compose up -d afterburner
 docker compose up -d afterburner-reactive
