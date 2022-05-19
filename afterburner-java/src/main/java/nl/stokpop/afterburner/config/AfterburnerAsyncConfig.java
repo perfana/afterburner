5f79ff21aebbf24beeedfc92ca633576eafa871b
Peter Paul Bakker <peter.paul.bakker@stokpop.nl>
2022-05-19 16:50:35 +0200

    added async exception experiment

--- afterburner-java/src/main/java/nl/stokpop/afterburner/config/AfterburnerAsyncConfig.java
+++ afterburner-java/src/main/java/nl/stokpop/afterburner/config/AfterburnerAsyncConfig.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import nl.stokpop.afterburner.AfterburnerProperties;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.cloud.sleuth.instrument.async.LazyTraceExecutor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
