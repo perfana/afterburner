27314ab1bbe7c70dc0461abea5bf05f41146ba0e
Peter Paul Bakker <peter.paul.bakker@stokpop.nl>
2022-07-08 15:36:43 +0200

    added wiremock-stub, bumped to spring boot 2.7.1

---
+++ afterburner-wiremock/src/main/java/nl/stokpop/afterburner/wiremock/AfterburnerStub.java
@@ -0,0 +1,22 @@
+package nl.stokpop.afterburner.wiremock;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
+
+import java.util.Scanner;
+
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+
+public class AfterburnerStub {
+    public static void main(String[] args) {
+        WireMockServer wireMockServer = new WireMockServer(wireMockConfig().port(8060));
+        wireMockServer.start();
+
+        Scanner s = new Scanner(System.in);
+        System.out.println("Press enter to continue.....");
+        s.nextLine();
+
+        WireMock.reset();
+        wireMockServer.stop();
+    }
+}
