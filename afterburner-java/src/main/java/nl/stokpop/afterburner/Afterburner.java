d68f879414fc3a0243e132a274ac9faf137f0095
Peter Paul Bakker <peter.paul.bakker@stokpop.nl>
2022-08-22 17:03:27 +0200

    version 2.1.10: bumped to spring boot 2.7.3, bumped other versions, no warn in startup logs

--- afterburner-java/src/main/java/nl/stokpop/afterburner/Afterburner.java
+++ afterburner-java/src/main/java/nl/stokpop/afterburner/Afterburner.java
@@ -1,17 +1,22 @@
 package nl.stokpop.afterburner;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+// exclude to avoid log WARN "with default user credentials"
+@SpringBootApplication(exclude = { UserDetailsServiceAutoConfiguration.class })
 @EnableAsync
 @EnableScheduling
 @ConfigurationPropertiesScan
 @OpenAPIDefinition
+// make specific mapper scan to avoid log: "WARN No MyBatis mapper was found in '[nl.stokpop.afterburner]' package."
+@MapperScan(basePackages = "nl.stokpop.afterburner.mybatis")
 public class Afterburner {
 
     public static void main(String[] args) {
