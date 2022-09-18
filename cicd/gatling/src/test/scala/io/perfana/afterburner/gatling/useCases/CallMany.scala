1e47f364bdd388e616e57b35278c2d095d26725c
Peter Paul Bakker <peter.paul.bakker@stokpop.nl>
2022-09-18 15:08:32 +0200

    gatling loadTest: increase target users from 2 to 10, decreased call-many delay from 3333 to 333 ms

--- cicd/gatling/src/test/scala/io/perfana/afterburner/gatling/useCases/CallMany.scala
+++ cicd/gatling/src/test/scala/io/perfana/afterburner/gatling/useCases/CallMany.scala
@@ -6,7 +6,7 @@ import io.gatling.http.Predef._
 object CallMany {
 
   val call = exec(http("remote_call_async")
-    .get("/remote/call-many?count=5&path=delay?duration=3333")
+    .get("/remote/call-many?count=5&path=delay?duration=333")
     .header("perfana-request-name", "remote_call_async")
     .header("perfana-test-run-id", "${testRunId}")
     .check(status.is(200)))
