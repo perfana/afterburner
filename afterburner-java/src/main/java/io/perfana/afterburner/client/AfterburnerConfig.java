4f4f8f4a6a5ce641d45136cbe89110ed145d8028
Daniel Moll <daniel@perfana.io>
2022-11-02 23:05:25 +0100

    use default http connection pool

--- afterburner-java/src/main/java/io/perfana/afterburner/client/AfterburnerConfig.java
+++ afterburner-java/src/main/java/io/perfana/afterburner/client/AfterburnerConfig.java
@@ -61,8 +61,7 @@ public class AfterburnerConfig {
                 .build();
 
         PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
-        connectionManager.setDefaultMaxPerRoute(connectionsMax);
-        connectionManager.setMaxTotal(connectionsMax);
+
 
         MicrometerHttpClientInterceptor interceptor = new MicrometerHttpClientInterceptor(Metrics.globalRegistry,
                 this::extractUriWithoutParamsAsString,
