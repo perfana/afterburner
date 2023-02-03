dfddd39a5744142cc774bac5a760e9160a2d7c5e
Daniel Moll <dmoll1974@gmail.com>
2023-02-03 18:01:13 +0100

    reset

--- afterburner-java/src/main/java/io/perfana/afterburner/controller/CpuBurner.java
+++ afterburner-java/src/main/java/io/perfana/afterburner/controller/CpuBurner.java
@@ -42,7 +42,7 @@ public class CpuBurner {
         long startTime = System.currentTimeMillis();
 
         // no variation: is no fun!
-        int funSize = (int) (matrixSize * (1.0 + (random.nextDouble() * 4.2)));
+        int funSize = matrixSize;
 
         log.info("Calculate magic matrix identity for matrix size [{}].", funSize);
 
