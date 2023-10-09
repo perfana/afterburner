31ab3d8d9df28c5233b323accbc259eb2531a60d
Proxy Developer <action@github.com>
2023-10-09 18:07:07 +0000

    Proxy Dev: make matrix calculation more variable

--- afterburner-java/src/main/java/io/perfana/afterburner/controller/CpuBurner.java
+++ afterburner-java/src/main/java/io/perfana/afterburner/controller/CpuBurner.java
@@ -41,8 +41,8 @@ public class CpuBurner {
 
         long startTime = System.currentTimeMillis();
 
-        // no variation: is no fun!
-        int funSize = matrixSize;
+        // some variation: is more fun!
+        int funSize = (int) (matrixSize * (1.0 + (random.nextDouble() * 4.0)));
 
         log.info("Calculate magic matrix identity for matrix size [{}].", funSize);
 
