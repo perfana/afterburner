95fab00da95dbe5bf2894a99ffdbe45c6b195a29
Peter Paul Bakker <peterpaul@perfana.io>
2023-12-19 16:47:43 +0100

    added new base image with jdk21 and aarch64 with gclib for pyroscope/async profiler

--- afterburner-java/scripts/create-pyroscope-java-jar.sh
+++ afterburner-java/scripts/create-pyroscope-java-jar.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/env bash
 set -e
 
-git clone https://github.com/pyroscope-io/pyroscope-java.git
-TEMP_CLONE_DIR="$(pwd)/pyroscope-java"
-cd pyroscope-java
+pwd
+curl -L -o src/main/jib/pyroscope.jar "https://github.com/grafana/pyroscope-java/releases/download/v0.12.2/pyroscope.jar"
 
-function finish() {
-  echo "cleanup"
-  # prevent: Cannot lock Java compile cache ([...]/javaCompile) as it has already been locked by this process.
-  ./gradlew --stop
-  cd ..
-  rm -rf "$TEMP_CLONE_DIR"
-}
-trap finish EXIT
+#git clone https://github.com/pyroscope-io/pyroscope-java.git
+#TEMP_CLONE_DIR="$(pwd)/pyroscope-java"
+#cd pyroscope-java
 
-./gradlew shadowJar
-mv agent/build/libs/pyroscope.jar ../../afterburner-java/src/main/jib/
+#function finish() {
+#  echo "cleanup"
+#  # prevent: Cannot lock Java compile cache ([...]/javaCompile) as it has already been locked by this process.
+#  ./gradlew --stop
+#  cd ..
+#  rm -rf "$TEMP_CLONE_DIR"
+#}
+#trap finish EXIT
+#
+#./gradlew shadowJar
+#mv agent/build/libs/pyroscope.jar ../../afterburner-java/src/main/jib/
 
