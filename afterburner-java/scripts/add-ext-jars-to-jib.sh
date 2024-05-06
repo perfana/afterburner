35c1dfc468a966990dd0e295788e1c9dabaa1f12
Peter Paul Bakker <peterpaul@perfana.io>
2024-05-06 22:31:27 +0200

    print ext jar versions

--- afterburner-java/scripts/add-ext-jars-to-jib.sh
+++ afterburner-java/scripts/add-ext-jars-to-jib.sh
@@ -4,15 +4,17 @@ set -e
 # download jars to be used at runtime and package them into the image
 
 if [ ! -f src/main/jib/pyroscope.jar ]; then
-  echo "Downloading pyroscope jar"
-  curl -Ss -L -o src/main/jib/pyroscope.jar "https://github.com/grafana/pyroscope-java/releases/download/v0.14.0/pyroscope.jar"
+  PYROSCOPE_VERSION="0.14.0"
+  echo "Downloading pyroscope jar version $PYROSCOPE_VERSION"
+  curl -Ss -L -o src/main/jib/pyroscope.jar "https://github.com/grafana/pyroscope-java/releases/download/v${PYROSCOPE_VERSION}/pyroscope.jar"
 else
   echo "Pyroscope jar already exists"
 fi
 
 if [ ! -f src/main/jib/jfr-exporter.jar ]; then
-  echo "Downloading JFR exporter jar"
-  curl -Ss -L -o src/main/jib/jfr-exporter.jar "https://github.com/perfana/jfr-exporter/releases/download/0.4.0/jfr-exporter-0.4.0.jar"
+  JFR_EXPORTER_VERSION=0.4.0
+  echo "Downloading JFR exporter jar version $JFR_EXPORTER_VERSION"
+  curl -Ss -L -o src/main/jib/jfr-exporter.jar "https://github.com/perfana/jfr-exporter/releases/download/${JFR_EXPORTER_VERSION}/jfr-exporter-${JFR_EXPORTER_VERSION}.jar"
 else
   echo "JFR exporter jar already exists"
 fi
