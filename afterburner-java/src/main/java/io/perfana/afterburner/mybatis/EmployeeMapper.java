1457aaac1e9fed8691cc1a4b694686c3b51ccfb6
Dylan van Iersel <dylan@perfana.io>
2024-05-19 15:04:45 +0200

    Add a query issue to the repertoire of ProxyDev. Also, introduce any issue only once every 4 builds.

--- afterburner-java/src/main/java/io/perfana/afterburner/mybatis/EmployeeMapper.java
+++ afterburner-java/src/main/java/io/perfana/afterburner/mybatis/EmployeeMapper.java
@@ -23,4 +23,28 @@ public interface EmployeeMapper {
     //@QueryHints(value = { @QueryHint(name = "javax.persistence.query.timeout", value = "20") })
     int selectLongTime(@Param("durationInSec") int durationInSec);
 
+    @Select("SELECT e.* " +
+            "FROM employees AS e " +
+            "         JOIN ( " +
+            "    SELECT emp_no " +
+            "    FROM dept_emp AS de1 " +
+            "    WHERE to_date = ( " +
+            "        SELECT MAX(to_date) " +
+            "        FROM dept_emp AS de2 " +
+            "        WHERE de1.emp_no = de2.emp_no " +
+            "    ) " +
+            ") AS subq ON e.emp_no = subq.emp_no " +
+            "         JOIN dept_emp AS de ON e.emp_no = de.emp_no " +
+            "         JOIN departments AS d ON de.dept_no = d.dept_no " +
+            "         JOIN salaries AS s ON e.emp_no = s.emp_no " +
+            "WHERE d.dept_no IN ( " +
+            "    SELECT dm.dept_no " +
+            "    FROM dept_manager dm " +
+            "             JOIN employees em ON dm.emp_no = em.emp_no" +
+            "    WHERE em.last_name = #{lastName}" +
+            "      AND dm.to_date = (SELECT MAX(to_date) FROM dept_manager WHERE dept_no = dm.dept_no) " +
+            ") " +
+            ";")
+    List<Employee> findEmployeesByManagerLastName(@Param("lastName") String lastName);
+
 }
