dae8a462993a733a8ef2ab5391cbf6e685d32b71
Peter Paul Bakker <peterpaul@perfana.io>
2024-07-03 15:58:19 +0200

    gatling test in cicd: added find-by-manager-last-name call with last name feeder

--- cicd/gatling/src/test/scala/io/perfana/afterburner/gatling/feeders/DatabaseFeeder.scala
+++ cicd/gatling/src/test/scala/io/perfana/afterburner/gatling/feeders/DatabaseFeeder.scala
@@ -9,10 +9,8 @@ import io.gatling.jdbc.Predef._
 
 import io.gatling.jdbc.Predef._
 
-/**
- * Created by x077411 on 12/12/2014.
- */
 object DatabaseFeeder {
 
   var mariaDb = jdbcFeeder("jdbc:mysql://afterburner-db-mysql-headless:3306/employees", "afterburner", System.getProperty("employeeDbPassword"), "SELECT DISTINCT first_name as FIRST_NAME from employees.employees;").random()
+
 }
