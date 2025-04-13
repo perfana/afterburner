6c1cdd634cf7d688ca9166ec99af07f936a8e1e1
Daniel Moll <dmoll1974@gmail.com>
2025-04-13 16:28:58 +0200

    Proxy Dev: make cpu more efficient

--- afterburner-java/src/main/java/io/perfana/afterburner/controller/CpuBurner.java
+++ afterburner-java/src/main/java/io/perfana/afterburner/controller/CpuBurner.java
@@ -41,8 +41,9 @@ public class CpuBurner {
 
         long startTime = System.currentTimeMillis();
 
-        // some variation: is more fun!
-        int funSize = (int) (matrixSize * (1.0 + (random.nextDouble() * 1.5)));
+        // no variation: is no fun!
+        int funSize = matrixSize;
+
         log.info("Calculate magic matrix identity for matrix size [{}].", funSize);
 
         long[][] simpleMagicSquare;
