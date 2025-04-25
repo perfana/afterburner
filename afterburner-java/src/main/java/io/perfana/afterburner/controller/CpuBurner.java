c6f1353793f11a5446e0badd162a66506af3b3d6
Daniel Moll <dmoll1974@gmail.com>
2025-04-25 09:33:06 +0200

    revert feat: show more details

--- afterburner-java/src/main/java/io/perfana/afterburner/controller/CpuBurner.java
+++ afterburner-java/src/main/java/io/perfana/afterburner/controller/CpuBurner.java
@@ -34,16 +34,17 @@ public class CpuBurner {
         this.tracer = tracer;
     }
 
-    @Operation(summary = "Spend some time on CPU doing some magic matrix calculations.")
+    @Operation(summary = "Spend some time on CPU doing some magik matrix calculations.")
     @GetMapping("/cpu/magic-identity-check")
     public BurnerMessage magicIdentityCheck(
             @RequestParam(value = "matrixSize", defaultValue = "10") int matrixSize) throws InvalidMatrixException {
 
         long startTime = System.currentTimeMillis();
 
-        // some variation: is more fun!
-        int funSize = (int) (matrixSize * (1.0 + (random.nextDouble() * 1.5)));
-        log.info("Calculate magic matrix identity for matrix size [{}].", funSize);
+        // no variation: is no fun!
+        int funSize = matrixSize;
+
+        log.info("Calculate magik matrix identity for matrix size [{}].", funSize);
 
         long[][] simpleMagicSquare;
         long[][] identitySquare;
